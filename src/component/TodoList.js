239ebf4979badb3f1663a2873c3e429601c395b2
unknown <azeemolatunji@gmail.com>
2022-01-10 22:25:44 +0100

    Adde event to all componet

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -4,7 +4,12 @@ import TodoItem from './TodoItems';
 class TodoList extends React.Component {
     render() { 
         return <ul>
-            {this.props.todos.map(todo=> (<TodoItem key={todo.id} todo={todo} />))}
+            {this.props.todos.map(todo=> (
+            <TodoItem 
+            key={todo.id}
+            todo={todo}
+            handleChangeProps= {this.props.handleChangeProps} 
+            deleteTodoProps= {this.props.deleteTodoProps}/>))}
         </ul>;
     }
 }
