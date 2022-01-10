c845727b689c2fedb1294cbf38d339ec72169c45
unknown <azeemolatunji@gmail.com>
2022-01-10 12:06:04 +0100

    Configure react, created components for the todo list

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoContainer from './component/TodoContainer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
