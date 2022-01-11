b0ece07a43fa2c51b620c36921b8249f82fc3609
unknown <azeemolatunji@gmail.com>
2022-01-11 07:48:03 +0100

    Completed styling of all components

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const Header = () => {
-    return <header>
-        <h1>
+    const headerStyle = {
+        padding: "20px 0",
+        lineHeight: "1.5em",
+    }
+    return ( 
+        <header style={headerStyle}>
+        <h1 style= {{
+            fontSize: "6rem",
+            fontWeight: "600",
+            marginBottom: "2rem",
+            lineHeight: "1em",
+            color: "#ececec",
+            textTransform: "lowercase",
+            textAlign: "center",
+        }}>
         To-do list
         </h1>
     </header>
+    )
 }
 
 export default Header;
--- src/component/TodoContainer.js
+++ src/component/TodoContainer.js
@@ -60,12 +60,16 @@ class TodoContainer extends React.Component {
   }
 
     render() {
-        return <div>
+        return (
+        <div className="container">
+            <div className="inner">
             <Header />
             <InputTodo addTodoProps= {this.addTodoItem} />
             <TodoList todos={this.state.todos} handleChangeProps={this.handleChange} 
             deleteTodoProps={this.delTodo}/>
-        </div>;
+        </div>
+        </div>
+        );
     }
 }
 
