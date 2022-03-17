4ee20a3f7cf3d0a19d31d4526b96b13682eb8c2e
unknown <azeemolatunji@gmail.com>
2022-03-17 08:22:42 +0100

    made changes

--- src/component/InputTodo.js
+++ src/component/InputTodo.js
@@ -5,11 +5,7 @@ class InputTodo extends Component {
         title: ""
     };
 
-    onChange = e => {
-      this.setState({
-         [e.target.name]: e.target.value,
-      })
-    }
+    onChange = e => this.setState({[e.target.name]: e.target.value,})
 
     handleSubmit = e => {
         e.preventDefault();
